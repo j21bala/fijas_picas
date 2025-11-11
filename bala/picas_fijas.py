803c67a84e9d430b9d9c9d7ab6424f13d18e7dcd
Juan Balaguera <146900241+j21bala@users.noreply.github.com>
2025-11-11 02:55:08 +0000

    función logica del juego

--- bala/picas_fijas.py
+++ bala/picas_fijas.py
@@ -6,7 +6,27 @@ digitos = list(numero)
 cadena = set(digitos)
 while len(digitos) == 4 and len(digitos) == len(cadena):
 
-    
+    #Definimos la primera función que establece la lógica de fijas y picas
+
+    def comprobar_picas_fijas(cadena, suposicion):
+        fijas = 0
+        picas = 0
+        for i in range (len(cadena)):
+            if cadena[i] == suposicion[i]:
+                fijas += 1
+            elif suposicion[i] in cadena:
+        
+                picas +=1
+                
+        return fijas, picas
+
+            
+
+
+
+
+
+
 
 
     
